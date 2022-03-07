bddf8e62a407cd5e4122ab6ff97459fac9f56c1a
Pain <holtbrenden@yahoo.com>
2022-03-07 15:12:33 -0700

    starter

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+const employee = require('./lib/employee');
